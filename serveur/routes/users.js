60cc5817b2225af23e6a457ae34b35d500c68f86
BadValou <valentin.croset@gmail.com>
2021-06-29 16:35:28 +0200

    validate login for resp and coach

--- serveur/routes/users.js
+++ serveur/routes/users.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const users = require('../localData/user')
+
+function validateLogin(req, res, state) {
+    if (!req.body || !req.body.mail || !req.body.password) {
+        return res.json("no")
+    }
+    let mail = req.body.mail
+    let password = req.body.password
+    let user = users.filter(e => e.mail == mail && e.state == state)
+    if (user) {
+        user = user[0]
+    }
+    if (user && user.password == password) {
+        res.json("ok")
+    } else {
+        res.json("no")
+    }
+}
+
+// POST /users/coach/login
+router.post('/coach/login', function (req, res, next) {
+    validateLogin(req, res, "coach")
 });
+
+// POST /users/responsable/login
+router.post('/responsable/login', function (req, res, next) {
+    validateLogin(req, res, "responsable")
+});
+
 
 module.exports = router;
