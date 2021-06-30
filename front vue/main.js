fc25b6305cc14d0b27b5d92e00d35eb7d7ab6b14
BadValou <valentin.croset@gmail.com>
2021-06-30 16:56:18 +0200

    no user login and fix

--- front vue/main.js
+++ front vue/main.js
@@ -5,11 +5,18 @@ let app = new Vue({
             login: false,
             formationId: 0,
             posteState: {
-
+                manager: 1,
+                formateur: 0, // il fait l'appel, voir le calendrier
+                coatch: 0 // voir si le travail marche bien
             }
         }
     },
     methods: {
+        setPosteState(m, f, c) {
+            this.posteState.manager = m
+            this.posteState.formateur = f
+            this.posteState.coatch = c
+        },
         showRegisterForm(id) {
             this.formationId = id;
         }
